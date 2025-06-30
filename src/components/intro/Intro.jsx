1e841adbc780c8fb39c4e687251a8b68bd46e82b
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-06-30 22:36:40 +0300

    uploaded cv on download button

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,9 +5,11 @@ const Intro = () => {
   return (
     <div className="intro_container">
       <h1 className="intro_name">Francisc Szasz - Frontend Engineer</h1>
-      <button className="intro_button">
-        <p>Download Resume</p>
-      </button>
+      <a href="/assets/Szasz-Francisc-CV-EN.pdf" download>
+        <button className="intro_button">
+          <p>Download Resume</p>
+        </button>
+      </a>
     </div>
   );
 };
