3d97b1ef45143df318ba1f2bc748666352ff7e13
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-06-30 22:21:53 +0300

    test

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div className="intro_container">
+      <h1 className="intro_name">Francisc Szasz - Frontend Engineer</h1>
+      <button className="intro_button">
+        <p>Download Resume</p>
+      </button>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h3>@Francisc Szasz</h3>
+      <ul>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Portfolio</li>
+        <li>Testimonials</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
